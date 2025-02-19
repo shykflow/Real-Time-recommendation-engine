bede7a5c333e179d21e17b2018ee69a327126ff0
shykflow <musaffa010@gmail.com>
2025-02-19 14:27:10 -0500

    Refactor __init__.py

---
+++ src/models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Machine Learning models for recommendation system
+Implements SVD, NMF, and hybrid algorithms
+"""
+
+from .recommendation_engine import RecommendationEngine
+
+__all__ = ["RecommendationEngine"]
